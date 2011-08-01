9e7d528f3572e0903e9e9ddac6b929d86d1965d0
Paul Betts <paul@paulbetts.org>
2011-08-01 15:53:08 -0700

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuClickOnceTest::Application.config.secret_token = 'acb9d96429438b8463e487e3adcce3d341f9ce6240a9c739539adb6f3b7d3e75ed569f30eb17eb6349e6a8e963d56dc650d17ed6bc49200b78ecb62354896ec3'
